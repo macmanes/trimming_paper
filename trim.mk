f3afdc7f477ec2d5fbd4cddc0c4876dec12e11e2
macmanes <macmanes@gmail.com>
2013-06-27 21:53:23 -0700

    2153

--- trim.mk
+++ trim.mk
@@ -1,17 +1,17 @@
 #!/usr/bin/make
 
-all: out_1.fastq.quality out_1.fastq raw.Trinity.fasta real.0.Trinity.fasta real.1.Trinity.fasta real.2.Trinity.fasta real.5.Trinity.fasta real.10.Trinity.fasta real.15.Trinity.fasta real.20.Trinity.fasta
+all: out_1.fastq.quality out_1.fastq raw.Trinity.fasta right.1.fq right.2.fq right.5.fq right.10.fq right.15.fq right.20.fq real.1.Trinity.fasta real.2.Trinity.fasta real.5.Trinity.fasta real.10.Trinity.fasta real.15.Trinity.fasta real.20.Trinity.fasta
 
 out_1.fastq.quality:out_1.fastq
 	perl $solexaqa/SolexaQA.pl -p 0.01 out_1.fastq
 	cp out_1.fastq.quality ~/Dropbox/
 	cp out_1.fastq.quality.pdf ~/Dropbox/
 	
-raw.Trinity.fasta:out_1.fastq out_2.fastq	
+raw.Trinity.fasta:out_1.fastq
 	$trinity/Trinity.pl --full_cleanup --seqType fq --JM 30G --left out_1.fastq  --right out_2.fastq  --CPU 8 --output raw
 
-real.0.Trinity.fasta real.1.Trinity.fasta real.2.Trinity.fasta real.5.Trinity.fasta real.10.Trinity.fasta real.15.Trinity.fasta real.20.Trinity.fasta: out_1.fastq out_2.fastq
-	for TRIM in 0 1 2 5 10 15 20; do \
+right.1.fq right.2.fq right.5.fq right.10.fq right.15.fq right.20.fq: out_1.fastq
+	for TRIM in 1 2 5 10 15 20; do \
 		java -Xmx30g -jar ~/software/trimmomatic-0.30.jar PE \
 		-phred33 -threads 12 \
 		out_1.fastq \
@@ -29,5 +29,9 @@ real.0.Trinity.fasta real.1.Trinity.fasta real.2.Trinity.fasta real.5.Trinity.fa
 		cp T.$$TRIM.pp.1.fq.quality.pdf ~/Dropbox/ ;\
 		cat T.$$TRIM.pp.1.fq T.$$TRIM.up.1.fq > left.$$TRIM.fq ; \
 		cat T.$$TRIM.pp.2.fq T.$$TRIM.up.2.fq > right.$$TRIM.fq ; \
+	done
+
+real.1.Trinity.fasta real.2.Trinity.fasta real.5.Trinity.fasta real.10.Trinity.fasta real.15.Trinity.fasta real.20.Trinity.fasta: out_1.fastq
+	for TRIM in 1 2 5 10 15 20; do \
 		/home/macmanes/trinityrnaseq_r2013-02-25/Trinity.pl --full_cleanup --seqType fq --JM 30G --left left.$$TRIM.fq --right right.$$TRIM.fq --CPU 8 --output real.$$TRIM ; \
 	done
