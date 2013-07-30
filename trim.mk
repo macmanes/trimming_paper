9334ea723942f58a36e2d72eb5fa7fb0eda3b072
Matthew MacManes <macmanes@gmail.com>
2013-07-30 15:03:30 -0700

    updating

--- trim.mk
+++ trim.mk
@@ -8,13 +8,13 @@ READ1=left.fastq
 READ2=right.fastq
 TRIMMOMATIC ?= $(shell which 'trimmomatic-0.30.jar')
 BCODES := /home/macmanes/Dropbox/barcodes.fa
-SOLEXA := /home/macmanes/SolexaQA_v.2.1/SolexaQA_v.2.1
+SOLEXA := /home/macmanes/software/
 TRINITY := /home/macmanes/trinityrnaseq-code/trunk
 MUS := /media/macmanes/hd/flux/genomes/mus/Mus_musculus.GRCm38.71.cdna.all.fa
 PFAM := /media/macmanes/raid/blastdb/Pfam-A.hmm
 
 
-all: subsamp trim trim1 trim2 trim3 trim4 trin trin1 trin2 trin3 trin4 pslx orf
+all: $(READ1).quality subsamp trim trim1 trim2 trim3 trim4 trin trin1 trin2 trin3 trin4 pslx orf
 
 
 $(READ1).quality:
@@ -26,30 +26,10 @@ $(READ1).quality:
 #	--left $(RUN) --right $(RUN) --group_pairs_distance 999 --CPU $(CPU) --output $(RUN)
 
 
-subsamp : $(READ1) $(READ2)
-	python ~/error_correction/scripts/subsampler.py 1000000 $(READ1) $(READ2)
-	mv subsamp_1.fastq raw.10M.$(READ1)
-	mv subsamp_2.fastq raw.10M.$(READ2)		
-subsamp : $(READ1) $(READ2)
+subsamp1 : $(READ1) $(READ2)
 	python ~/error_correction/scripts/subsampler.py 10000000 $(READ1) $(READ2)
 	mv subsamp_1.fastq raw.10M.$(READ1)
 	mv subsamp_2.fastq raw.10M.$(READ2)	
-subsamp : $(READ1) $(READ2)
-	python ~/error_correction/scripts/subsampler.py 20000000 $(READ1) $(READ2)
-	mv subsamp_1.fastq raw.20M.$(READ1)
-	mv subsamp_2.fastq raw.20M.$(READ2)	
-subsamp : $(READ1) $(READ2)
-	python ~/error_correction/scripts/subsampler.py 50000000 $(READ1) $(READ2)
-	mv subsamp_1.fastq raw.50M.$(READ1)
-	mv subsamp_2.fastq raw.50M.$(READ2)	
-subsamp : $(READ1) $(READ2)
-	python ~/error_correction/scripts/subsampler.py 75000000 $(READ1) $(READ2)
-	mv subsamp_1.fastq raw.75M.$(READ1)
-	mv subsamp_2.fastq raw.75M.$(READ2)	
-subsamp : $(READ1) $(READ2)
-	python ~/error_correction/scripts/subsampler.py 100000000 $(READ1) $(READ2)
-	mv subsamp_1.fastq raw.100M.$(READ1)
-	mv subsamp_2.fastq raw.100M.$(READ2)	
 
 trim: 
 	@echo About to start trimming
@@ -74,6 +54,10 @@ trin:
 		$(TRINITY)/Trinity.pl --full_cleanup --min_kmer_cov 2 --seqType fq --JM $(MEM)G --bflyHeapSpaceMax $(MEM)G  \
 		--left 10M.left.$$TRIM.fq --right 10M.right.$$TRIM.fq --group_pairs_distance 999 --CPU $(CPU) --output 10M.$$TRIM; rm 10M.left.$$TRIM.fq 10M.right.$$TRIM.fq; done
 
+subsamp2 : $(READ1) $(READ2)
+	python ~/error_correction/scripts/subsampler.py 20000000 $(READ1) $(READ2)
+	mv subsamp_1.fastq raw.20M.$(READ1)
+	mv subsamp_2.fastq raw.20M.$(READ2)	
 trim1: 
 	@echo About to start trimming
 	for TRIM in 2 5 10 20; do \
@@ -97,6 +81,10 @@ trin1:
 		$(TRINITY)/Trinity.pl --full_cleanup --min_kmer_cov 2 --seqType fq --JM $(MEM)G --bflyHeapSpaceMax $(MEM)G  \
 		--left 20M.left.$$TRIM.fq --right 20M.right.$$TRIM.fq --group_pairs_distance 999 --CPU $(CPU) --output 20M.$$TRIM; rm 10M.left.$$TRIM.fq 10M.right.$$TRIM.fq; done
 
+subsamp3 : $(READ1) $(READ2)
+	python ~/error_correction/scripts/subsampler.py 50000000 $(READ1) $(READ2)
+	mv subsamp_1.fastq raw.50M.$(READ1)
+	mv subsamp_2.fastq raw.50M.$(READ2)	
 trim2: 
 	@echo About to start trimming
 	for TRIM in 2 5 10 20; do \
@@ -120,6 +108,10 @@ trin2:
 		$(TRINITY)/Trinity.pl --full_cleanup --min_kmer_cov 2 --seqType fq --JM $(MEM)G --bflyHeapSpaceMax $(MEM)G  \
 		--left 50M.left.$$TRIM.fq --right 50M.right.$$TRIM.fq --group_pairs_distance 999 --CPU $(CPU) --output 50M.$$TRIM; rm 10M.left.$$TRIM.fq 10M.right.$$TRIM.fq; done
 
+subsamp4 : $(READ1) $(READ2)
+	python ~/error_correction/scripts/subsampler.py 75000000 $(READ1) $(READ2)
+	mv subsamp_1.fastq raw.75M.$(READ1)
+	mv subsamp_2.fastq raw.75M.$(READ2)	
 trim3: 
 	@echo About to start trimming
 	for TRIM in 2 5 10 20; do \
@@ -142,6 +134,10 @@ trin3:
 	for TRIM in 2 5 10 20; do \
 		$(TRINITY)/Trinity.pl --full_cleanup --min_kmer_cov 2 --seqType fq --JM $(MEM)G --bflyHeapSpaceMax $(MEM)G  \
 		--left 75M.left.$$TRIM.fq --right 75M.right.$$TRIM.fq --group_pairs_distance 999 --CPU $(CPU) --output 75M.$$TRIM; rm 10M.left.$$TRIM.fq 10M.right.$$TRIM.fq; done
+subsamp5 : $(READ1) $(READ2)
+	python ~/error_correction/scripts/subsampler.py 100000000 $(READ1) $(READ2)
+	mv subsamp_1.fastq raw.100M.$(READ1)
+	mv subsamp_2.fastq raw.100M.$(READ2)	
 trim4: 
 	@echo About to start trimming
 	for TRIM in 2 5 10 20; do \
